86363c75a7caa1faa9e9d3115ee3f06d4458c547
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-06-02 18:48:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,13 @@ resource "aws_instance" "ec2_example" {
 # The following null resource will print message "Hello World"
 
 resource "null_resource" "null_resource_simple" {
+  
+# This trigger will only execute once when it detects the instance id of EC2 instance 
+  triggers = {
+
+    id = aws_instance.ec2_example.id    # to execute it every time replace - id = time()
+
+  }
 
   provisioner "local-exec" {
 
